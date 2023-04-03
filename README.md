f240a47ca11214a89677fbd604a4bfc89492b663
Emerson Rocha <rocha@ieee.org>
2023-04-03 18:54:10 -0300

    scripts/govbrasil-ibge_estatisticas.py: rascunho inicial de gerar dados calculados de UF e municipios

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 ## Divisões político-administrativas
 
 - https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2022/Leia_me.pdf
+- (por verificar, aqui tem a nível intramunicipal)
+  - https://www.ibge.gov.br/geociencias/organizacao-do-territorio/malhas-territoriais/26565-malhas-de-setores-censitarios-divisoes-intramunicipais.html
--- scripts/govbrasil-ibge_estatisticas.py
+++ scripts/govbrasil-ibge_estatisticas.py
@@ -33,9 +33,10 @@ import geopandas
 import os
 import sys
 import argparse
+import csv
 
 # Fix "UserWarning: The Shapely GEOS version ... needs come before geopandas
-os.environ['USE_PYGEOS'] = '0'
+# os.environ['USE_PYGEOS'] = '0'
 
 
 # from frictionless import Package
@@ -58,33 +59,17 @@ __EPILOGUM__ = """
 Quickstart . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
     {0} --input-ibge-shapefile='data/ibge/BR_Municipios_2022.shp'
     USE_PYGEOS=0 {0} --input-ibge-shapefile='data/ibge/BR_Municipios_2022.shp'
-
-Validate file with cli . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
-(Use this if export does not work for some reason)
-    frictionless validate datapackage.json
-
-Create the datapackage.json (requires other tool) . . . . . . . . . . . . . . .
-(This command may be outdated eventually)
-    ./999999999/0/1603_1.py --methodus='data-apothecae' \
---data-apothecae-ad-stdout --data-apothecae-formato='datapackage' \
---data-apothecae-ex-suffixis='no1.tm.hxl.csv' \
---data-apothecae-ex-praefixis='1603_16,!1603_1_1,!1603_1_51' \
-> ./datapackage.json
-
-(same, but now all tables under 1603. Migth run out of memory) . . . . . . . . .
-    ./999999999/0/1603_1.py --methodus='data-apothecae' \
---data-apothecae-ad-stdout --data-apothecae-formato='datapackage' \
---data-apothecae-ex-suffixis='no1.tm.hxl.csv' \
---data-apothecae-ex-praefixis='1603' \
-> ./datapackage.json
-
-(Use jq to print resources)
-    jq .resources[].name < datapackage.json
+    USE_PYGEOS=0 {0} \
+--input-ibge-shapefile='data/ibge/BR_Municipios_2022.shp' \
+--input-ibge-nivel='municipio'
 
 ------------------------------------------------------------------------------
                             EXEMPLŌRUM GRATIĀ
 ------------------------------------------------------------------------------
 """.format(__file__)
+
+# USE_PYGEOS=0 ./scripts/govbrasil-ibge_estatisticas.py --input-ibge-shapefile='data/ibge/BR_Municipios_2022.shp' --input-ibge-nivel='municipio' > relatorio/_divisao-administrativa-municipio_ibge.hxl.csv
+# USE_PYGEOS=0 ./scripts/govbrasil-ibge_estatisticas.py --input-ibge-shapefile='data/ibge/BR_UF_2022.shp' --input-ibge-nivel='uf' > relatorio/_divisao-administrativa-uf_ibge.hxl.csv
 
 STDIN = sys.stdin.buffer
 
@@ -116,18 +101,18 @@ class Cli:
             '--input-ibge-shapefile',
             help='Caminho para o shapefile do IBGE',
             dest='ibge_shp',
-            # default='datapackage.json',
             nargs='?'
         )
 
-        # parser.add_argument(
-        #     '--excel',
-        #     help='Relative path and extension to the excel file.'
-        #     'Defaults to mdciii.xlsx on current directory',
-        #     dest='excel',
-        #     default='mdciii.xlsx',
-        #     nargs='?'
-        # )
+        parser.add_argument(
+            '--input-ibge-nivel',
+            help='Caminho para o shapefile do IBGE',
+            dest='ibge_nivel',
+            default='municipio',
+            choices=['municipio', 'uf'],
+            nargs='?'
+        )
+
         return parser.parse_args()
 
     def execute_cli(
@@ -136,26 +121,63 @@ class Cli:
     ):
         # frictionless_to_excel(pyargs.datapackage, pyargs.excel)
 
-        ibge_estatisticas(pyargs.ibge_shp)
+        if pyargs.ibge_nivel == 'municipio':
+            ibge_estatisticas_municipio(pyargs.ibge_shp)
+        elif pyargs.ibge_nivel == 'uf':
+            ibge_estatisticas_uf(pyargs.ibge_shp)
+        else:
+            raise SyntaxError
 
         # print('unknow option.')
         return self.EXIT_OK
 
 
-def ibge_estatisticas(path_shapefile: str):
-    # gdf_ibge = geopandas.read_file("data/tmp/BR_UF_2022.gpkg")
+def ibge_estatisticas_uf(path_shapefile: str):
     gdf_ibge = geopandas.read_file(path_shapefile)
 
-    print(">> resumo IBGE")
-    print(gdf_ibge)
+    # print(">> resumo IBGE")
+    # print(gdf_ibge)
 
-    gdf_ibge.to_crs(crs=3857)
+    # https://gis.stackexchange.com/questions/48949/epsg-3857-or-4326-for-googlemaps-openstreetmap-and-leaflet
+    gdf_ibge = gdf_ibge.to_crs(epsg=32723)
+    gdf_ibge_centroid = gdf_ibge.centroid.to_crs(epsg=4326)
 
-    print(gdf_ibge.area)
+    # @see https://cursos.alura.com.br/forum/topico-calculo-do-centroid-nao-funciona-129758
+    # print(gdf_ibge.area)
 
+    csvw = csv.writer(sys.stdout)
+    csvw.writerow(['CD_UF', 'NM_UF', 'SIGLA_UF', 'NM_REGIAO',
+                  'AREA_KM2', '_aream2', '_centroid'])
     for index, row in gdf_ibge.iterrows():
-        # print(index, row, row['geometry'].to_crs().area)
-        print(index, row, row['geometry'].area)
+
+        _x = gdf_ibge_centroid[index].x
+        _y = gdf_ibge_centroid[index].y
+        csvw.writerow([row['CD_UF'], row['NM_UF'], row['SIGLA_UF'], row['NM_REGIAO'],
+                       row['AREA_KM2'], row['geometry'].area, f"POINT({_x} {_y})"])
+
+
+def ibge_estatisticas_municipio(path_shapefile: str):
+    gdf_ibge = geopandas.read_file(path_shapefile)
+
+    # print(">> resumo IBGE")
+    # print(gdf_ibge)
+
+    # https://gis.stackexchange.com/questions/48949/epsg-3857-or-4326-for-googlemaps-openstreetmap-and-leaflet
+    gdf_ibge = gdf_ibge.to_crs(epsg=32723)
+    gdf_ibge_centroid = gdf_ibge.centroid.to_crs(epsg=4326)
+
+    # @see https://cursos.alura.com.br/forum/topico-calculo-do-centroid-nao-funciona-129758
+    # print(gdf_ibge.area)
+
+    csvw = csv.writer(sys.stdout)
+    csvw.writerow(['CD_MUN', 'NM_MUN', 'SIGLA_UF',
+                  'AREA_KM2', '_aream2', '_centroid'])
+    for index, row in gdf_ibge.iterrows():
+
+        _x = gdf_ibge_centroid[index].x
+        _y = gdf_ibge_centroid[index].y
+        csvw.writerow([row['CD_MUN'], row['NM_MUN'], row['SIGLA_UF'],
+                       row['AREA_KM2'], row['geometry'].area, f"POINT({_x} {_y})"])
 
 
 if __name__ == "__main__":
