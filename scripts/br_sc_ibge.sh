6a2735397cc0c65106f809c16b215f39fa7b84c7
Emerson Rocha <rocha@ieee.org>
2023-04-16 22:21:14 -0300

    ./scripts/br_sc_ibge.sh: gpkg_layer_to_geojson_on_tmp()

--- scripts/br_sc_ibge.sh
+++ scripts/br_sc_ibge.sh
@@ -25,7 +25,7 @@ set -e
 # @see https://geoftp.ibge.gov.br/cartas_e_mapas/bases_cartograficas_continuas/bc25/sc/versao2020/
 
 # ROOTDIR="$(pwd)"
-# TEMPDIR="$(pwd)/data/tmp"
+TEMPDIR="$(pwd)/data/tmp"
 CACHEDIR="$(pwd)/data/cache"
 # MEMDIR="/tmp"
 
@@ -76,34 +76,72 @@ download_and_cache() {
   printf "\t%40s\n" "${tty_green}${FUNCNAME[0]} FINISHED OKAY ${tty_normal}"
 }
 
-# #######################################
-# # Baixa geometrias
-# #
-# # Globals:
-# #   CACHEDIR
-# # Arguments:
-# #    id
-# #    url
-# # Outputs:
-# #
-# #######################################
-# data_ibge_sc_gpkg_download() {
-#   printf "\n\t%40s\n" "${tty_blue}${FUNCNAME[0]} STARTED ${tty_normal}"
+#######################################
+# Extrai uma camada de arquivo geopackage em arquivo geojson
+#
+# Globals:
+#   TEMPDIR
+# Arguments:
+#    geopackage_file
+#    geopackage_layer
+#    geojson_file
+# Outputs:
+#
+#######################################
+gpkg_layer_to_geojson_on_tmp() {
+  printf "\n\t%40s\n" "${tty_blue}${FUNCNAME[0]} STARTED ${tty_normal}"
 
-#   id="${1}"
-#   url="${2}"
+  in_geopackage_file="${1}"
+  in_geopackage_layer="${2}"
+  out_geojson_file="${3}"
 
-#   if [ ! -f "${CACHEDIR}/${id}.zip" ]; then
-#     set -x
-#     curl --output "${CACHEDIR}/${id}.zip" \
-#       "${url}"
-#     set +x
-#   else
-#     echo "Cacheado ${CACHEDIR}/${id}.zip"
-#   fi
+  if [ ! -f "${TEMPDIR}/${out_geojson_file}" ]; then
+    set -x
+    # curl --output "${CACHEDIR}/${local_file}" \
+    #   "${url}"
+    # ogr2ogr -f GeoJSON data/tmp/bc25_sc_2020-10-01__cbge_cemiterio_p.geojson data/tmp/bc25_sc_2020-10-01.gpkg cbge_cemiterio_p
+    ogr2ogr \
+      -f GeoJSON "${TEMPDIR}/${out_geojson_file}" \
+      "${TEMPDIR}/${in_geopackage_file}" \
+      "${in_geopackage_layer}"
+    set +x
+  else
+    echo "Cacheado [${TEMPDIR}/${out_geojson_file}]"
+  fi
 
-#   printf "\t%40s\n" "${tty_green}${FUNCNAME[0]} FINISHED OKAY ${tty_normal}"
-# }
+  printf "\t%40s\n" "${tty_green}${FUNCNAME[0]} FINISHED OKAY ${tty_normal}"
+}
+
+#######################################
+# Umzip
+#
+# Globals:
+#   CACHEDIR
+#   TEMPDIR
+# Arguments:
+#    cached_zip
+#    tmp_subdir
+#    example_internal_file
+# Outputs:
+#
+#######################################
+unzip_cached_on_tmp() {
+  printf "\n\t%40s\n" "${tty_blue}${FUNCNAME[0]} STARTED ${tty_normal}"
+
+  cached_zip="${1}"
+  tmp_subdir="${2}"
+  example_internal_file="${3}"
+
+  if [ ! -f "${TEMPDIR}/${tmp_subdir}${example_internal_file}" ]; then
+    set -x
+    unzip "${CACHEDIR}/${cached_zip}" -d "${TEMPDIR}/${tmp_subdir}/"
+    set +x
+  else
+    echo "Ja existia: [${TEMPDIR}/${tmp_subdir}${example_internal_file}]"
+  fi
+
+  printf "\t%40s\n" "${tty_green}${FUNCNAME[0]} FINISHED OKAY ${tty_normal}"
+}
 
 #### main ______________________________________________________________________
 
@@ -111,4 +149,20 @@ download_and_cache() {
 
 download_and_cache "bc25_sc_2020-10-01.zip" "${IBGE_SC_GPKG_ZIP_URL}"
 download_and_cache "bc25_sc_listanomesgeograficos.csv" "${IBGE_SC_LISTA_NOMES_CSV_URL}"
+
+unzip_cached_on_tmp "bc25_sc_2020-10-01.zip" "" "bc25_sc_2020-10-01.gpkg"
+
+gpkg_layer_to_geojson_on_tmp "bc25_sc_2020-10-01.gpkg" "cbge_cemiterio_p" "bc25_sc_2020-10-01__cbge_cemiterio_p.geojson"
+gpkg_layer_to_geojson_on_tmp "bc25_sc_2020-10-01.gpkg" "enc_torre_energia_p" "bc25_sc_2020-10-01__enc_torre_energia_p.geojson"
+
+# ogr2ogr -f GeoJSON data/tmp/bc25_sc_2020-10-01__cbge_cemiterio_p.geojson data/tmp/bc25_sc_2020-10-01.gpkg cbge_cemiterio_p
+
 # data_ibge_sc_gpkg_download "bc25_sc_2020-10-01" "${IBGE_SC_GPKG_ZIP_URL}"
+
+# ogrinfo data/tmp/bc25_sc_2020-10-01.gpkg
+# ogrinfo data/cache/bc25_sc_2020-10-01.zip
+# ogr2ogr data/cache/bc25_sc_2020-10-01.zip
+
+# ogr2ogr -f GeoJSON data/tmp/bc25_sc_2020-10-01.gpkg -nln cbge_cemiterio_p data/tmp/bc25_sc_2020-10-01__cbge_cemiterio_p.geojson
+# ogr2ogr -f GeoJSON data/tmp/bc25_sc_2020-10-01.gpkg data/tmp/bc25_sc_2020-10-01__cbge_cemiterio_p.geojson
+# ogr2ogr -f GeoJSON data/tmp/bc25_sc_2020-10-01__cbge_cemiterio_p.geojson -nln cbge_cemiterio_p data/tmp/bc25_sc_2020-10-01.gpkg
